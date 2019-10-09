c8b609a6e8f70e08775fd4f7bce1587720b8fbd1
Vitalii Yevtushenko <vitalii@rollncode.com>
2019-10-09 11:23:16 +0300

    init

---
+++ engineer-ai/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  engineer-ai
+//
+//  Created by Vitalii Yevtushenko on 08.10.2019.
+//  Copyright © 2019 ArcherSoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
